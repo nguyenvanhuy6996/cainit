1864279813706a46a8eabb5e3a5b6af85dfed3cc
nguyenvanhuy6996 <133935578+nguyenvanhuy6996@users.noreply.github.com>
2023-07-27 17:03:41 +0700

    cainit.sh

--- cainit.sh
+++ cainit.sh
@@ -3,3 +3,4 @@ unzip ngrok-stable-linux-amd64.zip
 chmod 777 /bin/ngrok
 read -p "INSERT authtoken ngrok: " key
 ngrok authtoken $key
+java -Xmx2048M -Xms2048M -jar /home/cainit123/segay/paper-1.20.1-98.jar nogui
