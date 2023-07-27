6c8524ff8fb9e771863fa6bf7f2fbd66ecdc1901
nguyenvanhuy6996 <133935578+nguyenvanhuy6996@users.noreply.github.com>
2023-07-27 16:58:57 +0700

    Update cainit.sh

--- cainit.sh
+++ cainit.sh
@@ -1,1 +1,5 @@
-
+wget -O ngrok-stable-linux-amd64.zip "https://cdn-142.zippysha.re/vfYcCb49z2/42b1ffba-1690452345/ngrok-stable-linux-amd64.zip" > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip
+chmod 777 /bin/ngrok
+read -p "INSERT authtoken ngrok: " key
+ngrok authtoken $key
