bc455f6070ad7507ac6d276bab9e83bd5f4eb6f8
nguyenvanhuy6996 <133935578+nguyenvanhuy6996@users.noreply.github.com>
2023-07-27 17:19:18 +0700

    Update cainit.sh

--- cainit.sh
+++ cainit.sh
@@ -1,6 +1,3 @@
-wget -O ngrok-stable-linux-amd64.zip "https://cdn-142.zippysha.re/vfYcCb49z2/42b1ffba-1690452345/ngrok-stable-linux-amd64.zip" > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip
-chmod 777 /bin/ngrok
-read -p "INSERT authtoken ngrok: " key
-ngrok authtoken $key
-java -Xmx2048M -Xms2048M -jar /home/cainit123/segay/paper-1.20.1-98.jar nogui
+wget -O ngrok-stable-linux-amd64.zip "https://github.com/KhanhNguyen9872/Debian_VPS_Google_Shell/blob/main/ngrok-stable-linux-amd64.zip?raw=true" > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1; rm ngrok-stable-linux-amd64.zip 2> /dev/null
+sudo mv ./ngrok /bin/ngrok; chmod 777 /bin/ngrok
